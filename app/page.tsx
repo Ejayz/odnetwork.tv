b12c964485c40d9e1dbef4438951fa48add6b28c
Aspect <32452006+aspectdev-ph@users.noreply.github.com>
2023-01-01 21:36:21 +0800

    Update API and Login page

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,12 @@ import Head from "next/head";
 import Image from "next/image";
 import React, { useSelector, useDispatch } from "react-redux";
 import { useRouter, usePathname, useSearchParams } from "next/navigation";
+import { useEffect, useRef, useState } from "react";
 
 export default function Page() {
+  const password = useRef(null);
   const router = useRouter();
+  const [showPassword,setshowPassword]=useState(false)
   const LoginUser = async (event: any) => {
     event.preventDefault();
     console.log(event);
@@ -33,6 +36,9 @@ export default function Page() {
       router.push("/faq");
     }
   };
+
+
+
   return (
     <>
       <Head>
@@ -68,11 +74,18 @@ export default function Page() {
                   </div>
                   <div className="mb-6">
                     <input
-                      type="password"
+                      type={showPassword?'text':'password'}
                       className="form-control m-0 block w-full rounded border border-solid border-gray-300 bg-white bg-clip-padding px-4 py-2 text-xl font-normal text-gray-700 transition ease-in-out focus:border-blue-600 focus:bg-white focus:text-gray-700 focus:outline-none"
                       placeholder="Password"
                       id="password"
                       name="password"
+                      ref={password}
+                    />
+                    <input
+                      type="checkbox"
+                      name="View Password"
+                      id="View_Password"
+                      onClick={()=>setshowPassword(!showPassword)}
                     />
                   </div>
 
--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,74 +1,75 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import * as dotenv from 'dotenv'
-import type { NextApiRequest, NextApiResponse } from 'next'
-import connection from './mysql'
-import * as bcrypt from "bcrypt"
-import jwt from "jsonwebtoken"
-import { json } from 'stream/consumers'
-import { connect } from 'http2'
-dotenv.config()
-var secret:any =process.env.JWT_KEY
-function login(query:string,email:any){
-  return new Promise((resolve,rejects)=>{
-    connection.query(query,[email],(err,result,feilds)=>{
-      if (err)
-      {
-        rejects(err)
-      } {
-         resolve(result)
+import * as dotenv from "dotenv";
+import type { NextApiRequest, NextApiResponse } from "next";
+import connection from "./mysql";
+import * as bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { json } from "stream/consumers";
+import { connect } from "http2";
+dotenv.config();
+var secret: any = process.env.JWT_KEY;
+function login(query: string, email: any) {
+  return new Promise((resolve, rejects) => {
+    connection.query(query, [email], (err, result, feilds) => {
+      if (err) {
+        rejects(err);
+        connection.end();
       }
-     
-      })
-      connection.end()
-   })
+      {
+        resolve(result);
+        connection.end();
+      }
+    });
+  });
 }
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
- if(req.body==null){
- res.statusCode=404
- res.status(404).end("Something went wrong")
- return
- }
-
-const email=req.body.email
-const password=req.body.password
-const remember_me=req.body.remember_me
- var query:string = "select EMAIL,USERNAME,USER_ID,YOUTUBE_CHANNEL_ID,WALLET_ID,PASSWORD,PROFILE_PIC from users_account where EMAIL=? and IS_EXIST='true'"
-return login(query,email).then((result:any)=>{
-  if(result.length==1){
-      bcrypt.compare(password, result[0].PASSWORD, function(err, resu) {
-    if(resu){
-      var token=jwt.sign({
-        "email":email,
-        "username":result[0].USERNAME,
-        "user_id":result[0].USER_ID,
-        "youtube_channel_id":result[0].YOUTUBE_CHANNEL_ID,
-        "wallet_id":result[0].WALLET_ID,
-        "profile_pic":result[0].PROFILE_PIC
-      },secret) 
-   res.status(200).json({
-        code:200,
-        token:token,
-        message:"Welcome back "+result[0].USERNAME
-      })
-
-    }else{
-      res.status(401).json({"code":401,
-    "message":"Username/Password do not match in our record!"})
-    }
-
-   
-});
-  }else{
-    res.status(401).json({"code":401,
-    "message":"Username/Password do not match in our record!"
-  })
- 
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.body == null) {
+    res.statusCode = 404;
+    res.status(404).end("Something went wrong");
+    return;
   }
 
-}).catch((e)=>{
-  res.status(500).json({"message":"something went wrong"})
-})
+  const email = req.body.email;
+  const password = req.body.password;
+  const remember_me = req.body.remember_me;
+  var query: string =
+    "select EMAIL,USERNAME,USER_ID,YOUTUBE_CHANNEL_ID,WALLET_ID,PASSWORD,PROFILE_PIC from users_account where EMAIL=? and IS_EXIST='true'";
+  return login(query, email)
+    .then((result: any) => {
+      if (result.length == 1) {
+        bcrypt.compare(password, result[0].PASSWORD, function (err, resu) {
+          if (resu) {
+            var token = jwt.sign(
+              {
+                email: email,
+                username: result[0].USERNAME,
+                user_id: result[0].USER_ID,
+                youtube_channel_id: result[0].YOUTUBE_CHANNEL_ID,
+                wallet_id: result[0].WALLET_ID,
+                profile_pic: result[0].PROFILE_PIC,
+              },
+              secret
+            );
+            res.status(200).json({
+              code: 200,
+              token: token,
+              message: "Welcome back " + result[0].USERNAME,
+            });
+          } else {
+            res.status(401).json({
+              code: 401,
+              message: "Username/Password do not match in our record!",
+            });
+          }
+        });
+      } else {
+        res.status(401).json({
+          code: 401,
+          message: "Username/Password do not match in our record!",
+        });
+      }
+    })
+    .catch((e) => {
+      res.status(500).json({ message: "something went wrong" });
+    });
 }
